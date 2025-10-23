db27cb484fa7043847677cb3a8ae1c9eb0bee2a3
Charan-teja16 <22r11a05x7@gcet.edu.in>
2025-10-23 20:15:11 +0530

    Update otp.py

--- backend/app/utils/otp.py
+++ backend/app/utils/otp.py
@@ -7,12 +7,13 @@ def send_otp_email(receiver_email: str) -> str:
 
     msg = MIMEText(f"Your OTP code is {otp}")
     msg["Subject"] = "Your Verification OTP"
-    msg["From"] = "project.test.g16@gmail.com"
+    msg["From"] = ""
     msg["To"] = receiver_email
 
     # ✅ Gmail SMTP (enable "App Passwords" in your Gmail account)
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login("project.test.g16@gmail.com", "xceo axzb secu rska")
-        server.sendmail("project.test.g16@gmail.com", receiver_email, msg.as_string())
+        server.login("", "")
+        server.sendmail("", receiver_email, msg.as_string())
 
     return otp
+
